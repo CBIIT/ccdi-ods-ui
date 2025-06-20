cc88cf5db5606bb294cabb17bf0047772f0cdd56
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2025-06-20 17:02:07 -0400

    Maked HeroCard for loop

--- src/components/LandingPage/Banner/index.tsx
+++ src/components/LandingPage/Banner/index.tsx
@@ -2,6 +2,21 @@ import React from "react";
 import HeroCard from "./Card";
 import ContactLink from "./ContactLink";
 
+const heroCards = [
+  {
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/1ca83a25a30597f6af82dd800925958c47b1a540?placeholderIfAbsent=true",
+    description: "Advise on programs that define impactful data and resources",
+  },
+  {
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/c5ebc61329056fc225979f14fc23306f1c00dc1d?placeholderIfAbsent=true",
+    description: "Implement guidance that promote broad access and use of data",
+  },
+  {
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/508050019b34ef9d3b387d9ce5a476f04365c72d?placeholderIfAbsent=true",
+    description: "Offer tools and processes that help ensure data are Findable, Accessable, Interoperable, and Resuable (FAIR).",
+  },
+];
+
 const Banner: React.FC = () => {
   return (
     <section 
@@ -16,24 +31,15 @@ const Banner: React.FC = () => {
       </h2>
       
       <div className="flex items-center gap-[25px] justify-center flex-wrap mt-[49px] max-md:max-w-full max-md:mt-10">
-        <HeroCard
-          imageSrc="https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/1ca83a25a30597f6af82dd800925958c47b1a540?placeholderIfAbsent=true"
-          description="Advise on programs that define impactful data and resources"
-        />
-        
-        <HeroCard
-          imageSrc="https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/c5ebc61329056fc225979f14fc23306f1c00dc1d?placeholderIfAbsent=true"
-          description="Implement guidance that promote broad access and use of data"
-          imageWidth="w-[74px]"
-          imageAspect="aspect-[0.8]"
-        />
-        
-        <HeroCard
-          imageSrc="https://cdn.builder.io/api/v1/image/assets/4ccc52d59fb54340b43b3652db0442b6/508050019b34ef9d3b387d9ce5a476f04365c72d?placeholderIfAbsent=true"
-          description="Offer tools and processes that help ensure data are Findable, Accessable, Interoperable, and Resuable (FAIR)."
-          imageWidth="w-[72px]"
-          imageAspect="aspect-[0.92]"
-        />
+        {heroCards.map((card, idx) => (
+          <HeroCard
+            key={idx}
+            imageSrc={card.imageSrc}
+            description={card.description}
+            imageWidth="w-[74px]"
+            imageAspect="aspect-[0.8]"
+          />
+        ))}
       </div>
       
       <div className="flex gap-3 flex-wrap mt-[68px] max-md:max-w-full max-md:mt-10">
