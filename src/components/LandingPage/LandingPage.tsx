ad3b5d507337a50dd82822399521861a988c72f7
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2025-06-25 16:45:32 -0400

    comment out useless component

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -12,7 +12,7 @@ export function LandingPage() {
       <Banner />
       <Gallery />
       <DataSharingGuidance />
-      <DataSharingProcess />
+      {/* <DataSharingProcess /> */}
       <DataSharingResources />
     </>
   );
