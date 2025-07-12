04d94109d2d4b6052aadb28717aeecb9d9cbceac
Chen <jonkiky@gmail.com>
2025-07-12 08:26:07 -0400

    add adobe launch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Geist, Geist_Mono, Open_Sans, Poppins, Lato, Inter, Nunito_Sans, Nunito, Public_Sans, Rubik } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,7 +59,72 @@ const rubik = Rubik({
 
 export const metadata: Metadata = {
   title: "NCI Data Sharing Hub",
-  description: "NCI Data Sharing Hub",
+  description: "Explore the NCIs data sharing and data access policies, guidelines, and resources to support cancer research and open science.",
+  metadataBase: new URL("https://datascience.cancer.gov"),
+  alternates: {
+    canonical: "/data-sharing"
+  },
+  openGraph: {
+    title: "Data Sharing - NCI Data Science",
+    description: "Explore the NCIs data sharing and data access policies, guidelines, and resources to support cancer research and open science.",
+    url: "https://datascience.cancer.gov/data-sharing",
+    siteName: "NCI Data Science",
+    type: "website",
+    images: [{
+      url: "https://datascience.cancer.gov/sites/default/files/styles/cgov_social_media/public/2023-01/data-sharing-header.jpg"
+    }]
+  },
+  twitter: {
+    card: "summary",
+    title: "Data Sharing - NCI Data Science",
+    description: "Explore the NCIs data sharing and data access policies, guidelines, and resources to support cancer research and open science."
+  },
+  icons: {
+    icon: [
+      {
+        url: "https://www.cancer.gov/profiles/custom/cgov_site/themes/custom/cgov/static/images/design-elements/icons/favicons/favicon.ico",
+        sizes: "32x32"
+      },
+      {
+        url: "https://www.cancer.gov/profiles/custom/cgov_site/themes/custom/cgov/static/images/design-elements/icons/favicons/favicon.svg",
+        type: "image/svg+xml"
+      }
+    ],
+    apple: [
+      {
+        url: "https://www.cancer.gov/profiles/custom/cgov_site/themes/custom/cgov/static/images/design-elements/icons/favicons/apple-touch-icon.png"
+      }
+    ],
+    other: [
+      {
+        rel: "manifest",
+        url: "https://www.cancer.gov/profiles/custom/cgov_site/themes/custom/cgov/static/images/design-elements/icons/favicons/site.webmanifest"
+      }
+    ]
+  },
+  other: {
+    "x-ua-compatible": "IE=edge",
+    "content-language": "en",
+    "dcterms.subject": "NCI Data Sharing",
+    "dcterms.type": "DataSharingLanding",
+    "dcterms.coverage": "nciglobal,ncienterprise",
+    "dcterms.isPartOf": "NCI Data Science",
+    "cgdp.domain": "datascience",
+    "MobileOptimized": "width",
+    "HandheldFriendly": "true"
+  },
+  viewport: {
+    width: "device-width",
+    userScalable: true,
+    initialScale: 1,
+    minimumScale: 1
+  }
+};
+
+// Add Adobe DTM script configuration
+export const adobeDTM = {
+  src: "https://assets.adobedtm.com/6a4249cd0a2c/785de09de161/launch-70d67a6a40a8.min.js",
+  async: true
 };
 
 export default function RootLayout({
@@ -68,6 +134,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script {...adobeDTM} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${openSans.variable} ${poppins.variable} ${lato.variable} ${inter.variable} ${nunitoSans.variable} ${nunito.variable} ${publicSans.variable} ${rubik.variable} antialiased bg-white min-h-screen`}
       >
