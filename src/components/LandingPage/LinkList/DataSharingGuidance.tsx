a62a11c1140782e58ba4cc619d8fbc16cd138220
Subhash Vallala <sgvallala@gmail.com>
2025-07-25 10:27:15 -0400

    DATASHARE-43 #17

--- src/components/LandingPage/LinkList/DataSharingGuidance.tsx
+++ src/components/LandingPage/LinkList/DataSharingGuidance.tsx
@@ -36,7 +36,7 @@ export const DataSharingGuidance: React.FC<DataSharingGuidanceProps> = ({ data }
   };
 
   return (
-    <section className="flex flex-col items-stretch items-center px-20 py-14 max-md:px-5 max-w-[1444px] mx-auto" >
+    <section className="flex flex-col items-stretch items-center px-20 py-14 pt-[40px] max-md:px-5 max-w-[1444px] mx-auto" >
       <div className="flex flex-col gap-8 ml-2.5 mb-4 w-full">
         {config.map((section: LinkListArrayConfig, sectionIdx: number) => (
           <React.Fragment key={sectionIdx}>
