08022cb27a5313918e5f860afb7caeee8cf720d0
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2025-10-15 21:33:17 -0400

    Fixed NCI logo stretched issue

--- src/components/Header/components/LogoTablet.tsx
+++ src/components/Header/components/LogoTablet.tsx
@@ -5,7 +5,7 @@ import { headerData } from '../../../config/globalHeaderData';
 const Logo = () => (
   <div className="flex">
     <a id="header-logo-home-link" className="mt-[32px]" href={headerData.globalHeaderLogoLink}>
-      <Image className="h-[50px] max-w-[350px]" src={headerData.globalHeaderLogoSmall} alt={headerData.globalHeaderLogoAltText} />
+      <Image className="h-[50px] max-w-[320px]" src={headerData.globalHeaderLogoSmall} alt={headerData.globalHeaderLogoAltText} />
     </a>
   </div>
 );
