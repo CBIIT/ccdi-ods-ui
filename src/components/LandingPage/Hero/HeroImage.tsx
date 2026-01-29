d60954f11c74b2d64f97c2e91bf1922822026c89
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2026-01-29 12:36:42 -0500

    Hero mobile

--- src/components/LandingPage/Hero/HeroImage.tsx
+++ src/components/LandingPage/Hero/HeroImage.tsx
@@ -9,14 +9,38 @@ interface HeroImageProps {
 
 export const HeroImage: React.FC<HeroImageProps> = ({ src, alt = "Hero illustration" }) => {
   return (
-    <div className="absolute top-[0px] right-[0px] flex w-full">
-      <Image
-        src={src}
-        alt={alt}
-        className="aspect-[1.28] object-contain h-[535px] max-w-full"
-        width={685}
-        height={535}
-      />
-    </div>
+    <>
+      {/* Desktop Image - shown on lg and above */}
+      <div className="hidden lg:block absolute top-[0px] right-[0px] flex w-full">
+        <Image
+          src={src}
+          alt={alt}
+          className="aspect-[1.28] object-contain h-[535px] max-w-full"
+          width={685}
+          height={535}
+        />
+      </div>
+      {/* Tablet Image - shown only on tablet (md to lg) */}
+      <div className="hidden md:block lg:hidden relative w-full">
+        <Image
+          src="/hero_tablet.svg"
+          alt={alt}
+          className="aspect-[1.28] object-contain h-[535px] w-full"
+          width={685}
+          height={535}
+        />
+      </div>
+      {/* Mobile Image - shown on mobile (below md) */}
+      <div className="block md:hidden relative w-full flex justify-center">
+        <Image
+          src="/hero_mobile.svg"
+          alt={alt}
+          className="object-contain w-full h-auto max-w-[358px]"
+          width={685}
+          height={535}
+          priority
+        />
+      </div>
+    </>
   );
 };
