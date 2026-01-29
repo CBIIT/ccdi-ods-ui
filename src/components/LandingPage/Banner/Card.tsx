1eb7613eb777820468b9942503562019188e35ff
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2026-01-28 22:54:00 -0500

    Green band responsive

--- src/components/LandingPage/Banner/Card.tsx
+++ src/components/LandingPage/Banner/Card.tsx
@@ -15,7 +15,7 @@ export const Card: React.FC<CardProps> = ({
   imageAspect = "aspect-[1]",
 }) => {
   return (
-    <div className="bg-transparent self-stretch flex min-w-60 max-w-[370px] items-center text-lg text-white font-normal text-center leading-6 p-2.5 rounded-[20px] ">
+    <div className="bg-transparent self-stretch flex min-w-60 max-w-[370px] items-center text-lg text-white font-normal text-center leading-6 p-2 rounded-[20px] lg:flex-1 lg:min-w-0 xl:flex-initial xl:min-w-60">
       <div className="self-stretch flex items-center gap-2.5 overflow-hidden my-auto px-2.5">
         <Image
           src={imageSrc}
@@ -25,7 +25,7 @@ export const Card: React.FC<CardProps> = ({
           className={`${imageAspect} object-contain ${imageWidth} self-stretch shrink-0 my-auto`}
         />
       </div>
-      <div className="text-white text-left [font-family:Nunito] text-[18px] font-normal leading-[24px] self-stretch min-w-60 w-[285px] my-auto px-[7px] py-2">
+      <div className="text-white text-left [font-family:Nunito] text-[18px] font-normal leading-[24px] self-stretch min-w-60 w-[270px] my-auto px-[7px] py-2 lg:min-w-[235px] lg:w-auto lg:flex-1 xl:min-w-60 xl:w-[270px]">
         {description}
       </div>
     </div>
